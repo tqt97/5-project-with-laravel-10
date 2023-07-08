aa7108f0e5dd32a6c9a8cc81196c22caa02fb767
QuocTuanIT <kutuanonline199@gmail.com>
2023-07-08 16:11:52 +0700

    add model and factory Task

--- task-list/database/seeders/DatabaseSeeder.php
+++ task-list/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Task;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +14,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // \App\Models\User::factory(10)->create();
+        Task::factory(10)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
